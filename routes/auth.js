0678adec4304ba523a4707a01ddb0c3e63c08499
ali00samy <ali123samy@gmail.com>
2021-04-24 23:17:44 +0200

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const Joi = require('joi');
 const bcryptjs = require('bcryptjs');
 const _ = require('lodash');
-const {User} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const {User, validate} = require('../models/user');
