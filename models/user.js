2162b0155484e5245401bd102b0023da571629cb
ali00samy <ali123samy@gmail.com>
2021-05-10 05:09:53 +0200

    change

--- models/user.js
+++ models/user.js
@@ -25,10 +25,14 @@ const userSchema = new mongoose.Schema({
   phone: {
     type: String,
     required: true,
+    minlength: 5,
+    maxlength: 1024
   },
   gendre : {
     type: String,
-    require: true
+    required: true,
+    minlength: 5,
+    maxlength: 1024
   },
   isAdmin: Boolean
 });
@@ -44,7 +48,9 @@ function validateUser(user) {
   const schema = {
     name: Joi.string().min(5).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(255).required()
+    password: Joi.string().min(5).max(255).required(),
+    phone: Joi.string().min(5).max(255).required(),
+    gendre: Joi.string().min(5).max(255).required(),
   };
 
   return Joi.validate(user, schema);
